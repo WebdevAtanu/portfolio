9eb0cee3a18a0243744f97b001cdbe14c8c4e652
Atanu <atanum991@gmail.com>
2024-07-29 12:17:44 +0530

    minor changes

--- src/comps/Nav.jsx
+++ src/comps/Nav.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {NavLink} from 'react-router-dom'
 
 function Nav() {
+  const [flag,setFlag]=useState(false);
 	const mode=()=> {
+    setFlag(!flag);
     let docBody = document.body;
     docBody.classList.toggle("dark_mode");
 	}
@@ -12,10 +14,10 @@ function Nav() {
       <div className="container-fluid">
         <div>
         <ul className='d-flex list-unstyled align-items-center justify-content-end gap-3'>
-          <li><h5><NavLink to='/' style={({isActive})=>({color:isActive?'#8169df':'#00BCD4'})}>INTRO</NavLink></h5></li>
-          <li><h5><NavLink to='/skill' style={({isActive})=>({color:isActive?'#8169df':'#00BCD4'})}>ABOUT</NavLink></h5></li>
-          <li><h5><NavLink to='/project' style={({isActive})=>({color:isActive?'#8169df':'#00BCD4'})}>PROJECTS</NavLink></h5></li>
-          <li><button onClick={mode}><i className="bi bi-moon-stars"></i></button></li>
+          <li><h5><NavLink to='/' className={({isActive})=>(isActive? 'active':'notactive')}>INTRO</NavLink></h5></li>
+          <li><h5><NavLink to='/skill' className={({isActive})=>(isActive? 'active':'notactive')}>ABOUT</NavLink></h5></li>
+          <li><h5><NavLink to='/project' className={({isActive})=>(isActive? 'active':'notactive')}>PROJECTS</NavLink></h5></li>
+          <li><button onClick={mode}>{flag?<i className="bi bi-sun"></i>:<i className="bi bi-moon-stars"></i>}</button></li>
         </ul>
         </div>
       </div>
