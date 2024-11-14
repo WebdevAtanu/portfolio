52e22a1163c78ae81e31e12a6909b3a2d2a1691d
Atanu <atanum991@gmail.com>
2024-11-14 11:18:16 +0530

    update

--- src/comps/Intro.jsx
+++ src/comps/Intro.jsx
@@ -3,31 +3,54 @@ import Content from './Content';
 import Skill from './Skill';
 function Intro() {
 return (
-    <>
-    <div className="flex gap-3 mb-3 items-center">
-      <div className="avatar">
-        <div className="ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-          <img src="pics/me.jpg" />
+<>
+<dialog id="my_image" className="modal">
+<div className="modal-box">
+  <div className="flex flex-col justify-center items-center">
+    <img src="pics/me.jpg" className='w-1/3 rounded-lg'/>
+    <p className='mt-1 text-xl'>Atanu Mondal</p>
+    <h6 className='text-sm text-gray-500 mb-1'>React.js || Web application || MERN stack</h6>
+    <div className="w-full grid grid-cols-3 gap-2">
+      <a href="CV.pdf" target="_blank">
+        <div className="flex flex-col items-center border border-black text-sm p-1 rounded-lg hover:bg-base-200">
+          <i className="bi bi-file-earmark-person-fill"></i>
+          <p>Resume</p>
         </div>
-      </div>
-      <div>
-        <h6 className="text-sm text-gray-500">Hey there, I'm</h6>
-        <h1 className="m-0 text-3xl">Atanu Mondal</h1>
-        <h6 className='text-sm text-gray-500'>React.js || Web application || MERN stack</h6>
-      </div>
+      </a>
+      <a href="https://www.linkedin.com/in/atanu-mondal-b98707255/" target="_blank">
+        <div className="flex flex-col items-center border border-black text-sm p-1 rounded-lg hover:bg-base-200">
+          <i className="bi bi-linkedin"></i>
+          <p>LinkedIn</p>
+        </div>
+      </a>
+      <a href="https://github.com/WebdevAtanu" target="_blank">
+        <div className="flex flex-col items-center border border-black text-sm p-1 rounded-lg hover:bg-base-200">
+          <i className="bi bi-github"></i>
+          <p>GitHub</p>
+        </div>
+      </a>
     </div>
-    <div className="flex flex-col gap-2 justify-between">
-      
-      <div className="flex gap-6 mb-3 items-center">
-        <a href="CV.pdf" target="_blank" download><button className="border bg-slate-800 hover:bg-slate-900 px-4 py-2 rounded text-white text-sm">View CV</button></a>
-        <a className='scale-[1.8] hover:text-blue-900 duration-150' href="https://www.linkedin.com/in/atanu-mondal-b98707255/" target="_blank"><i className="bi bi-linkedin"></i></a>
-        <a className='scale-[1.8] hover:text-black duration-150' href="https://github.com/WebdevAtanu" target="_blank"><i className="bi bi-github"></i></a>
-      </div>
-      <p>I'm a Front-end developer with a solid foundation in backend technologies. Proficient in creating engaging and responsive user interfaces using HTML, CSS, and JavaScript frameworks like React. Leverage backend knowledge in Node.js and Express to seamlessly integrate APIs and enhance application functionality. Experienced in working with databases to ensure smooth data management and flow. Passionate about delivering high-quality, user-friendly solutions while continuously learning and adapting to new technologies. Adept at collaborating in team environments to bring innovative ideas to life.</p>
+  </div>
+</div>
+<form method="dialog" className="modal-backdrop">
+  <button>close</button>
+</form>
+</dialog>
+<div className="flex gap-3 mb-3 items-center">
+  <div className="avatar cursor-pointer" onClick={()=>document.getElementById('my_image').showModal()}>
+    <div className="ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+      <img src="pics/me.jpg" />
     </div>
-    <Content/>
-    <Skill/>
-    </>
+  </div>
+  <div>
+    <h6 className="text-sm text-gray-500">Hey there, I'm</h6>
+    <h1 className="m-0 text-3xl">Atanu Mondal</h1>
+  </div>
+</div>
+<p>I'm a Front-end developer with a solid foundation in backend technologies. Proficient in creating engaging and responsive user interfaces using HTML, CSS, and JavaScript frameworks like React. Leverage backend knowledge in Node.js and Express to seamlessly integrate APIs and enhance application functionality. Experienced in working with databases to ensure smooth data management and flow. Passionate about delivering high-quality, user-friendly solutions while continuously learning and adapting to new technologies. Adept at collaborating in team environments to bring innovative ideas to life.</p>
+<Content/>
+<Skill/>
+</>
 )
 }
 export default Intro
