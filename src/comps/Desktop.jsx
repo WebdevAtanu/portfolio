8e00c51ee2db88ad0f06422f76ae481be5b2f5dc
Atanu <atanum991@gmail.com>
2024-12-11 13:37:01 +0530

    update

--- src/comps/Desktop.jsx
+++ src/comps/Desktop.jsx
@@ -2,27 +2,29 @@ import React from 'react'
 
 function Desktop(props) {
 	return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mb-5 p-3 shadow">
+    <>
+    <h5 className='text-xl mb-2 font-bold'>{props.h5}</h5>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mb-5 p-3">
       <div className="mockup-browser bg-slate-900 text-white border border-black rounded">
-        <div className="mockup-browser-toolbar">
-          <div className="input text-black">{props.href}</div>
+        <div className="mockup-browser-toolbar p-2">
+          <div className="input text-black border-black">{props.href}</div>
         </div>
-        <img src={props.src} alt="" className='w-full'/>
+        <div className="flex gap-4 overflow-x-auto">
+        <img src={props.img1} alt="" className='w-full'/>
+        <img src={props.img2} alt="" className='w-full'/>
+        <img src={props.img3} alt="" className='w-full'/>
+        </div>
       </div>
-      <div className='py-3 flex flex-col justify-between gap-4'>
-        <div>
-          <h5 className='text-xl mb-2'>{props.h5}</h5>
+      <div className='flex flex-col gap-4'>
           <p>{props.p}</p>
-          <p className='text-sm mt-2'>TechStack Used: {props.tech}</p>
-        </div>
-        <div>
-          <div className="flex gap-5">
-          <a href={props.href} target='_blank' className='underline underline-offset-4 hover:text-blue-800'><i className="bi bi-browser-chrome"></i> Live Link</a>
-          <a href={props.github} target='_blank' className='underline underline-offset-4 hover:text-blue-800'><i className="bi bi-github"></i> GitHub Link</a> 
+          <p className='text-sm'>TechStack Used: {props.tech}</p>
+          <div className="flex gap-3">
+          <a href={props.href} target='_blank' className='border border-black p-1 rounded hover:text-white hover:bg-slate-700'><i className="bi bi-browser-chrome"></i> Live Link</a>
+          <a href={props.github} target='_blank' className='border border-black p-1 rounded hover:text-white hover:bg-slate-700'><i className="bi bi-github"></i> GitHub Link</a> 
           </div>
-        </div>
       </div>
-    </div>
+      </div>
+    </>
 	)
 }
 
